3499c12173391fda0d1639fe0d15759cb71aae1e
Zhanna <zhuzha.jukova@yandex.ru>
2022-09-06 22:10:04 +0700

    Add solution for Task

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,5 +1,4 @@
-﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше, или равна 3 символа.
-string[] array1 = new string[5] {"task", "354", "array", "21", "count"};
+﻿string[] array1 = new string[5] {"111", "25", "task", "1", "count"};
 string[] array2 = new string[array1.Length];
 void SecondArray(string[] array1, string[] array2)
 {
